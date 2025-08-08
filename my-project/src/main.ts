b7d495f2462589cbd5371f73bdae4707585e2889
Roland Szucs <rolandszucs.private@gmail.com>
2025-08-08 08:24:21 +0200

    less-more buttons functionality TS

--- my-project/src/main.ts
+++ my-project/src/main.ts
@@ -1,5 +1,24 @@
-import './mobile.scss'
-import '../src/style components/daytime.scss'
-import '../src/style components/nighttime.scss'
+import '../src/mobile.scss'
+import '../src/mixins/daytime.scss';
+import '../src/mixins/nighttime.scss';
+
+const moreButton = document.getElementById('more-button') as HTMLButtonElement;
+const lessButton = document.getElementById('less-button') as HTMLButtonElement;
+const moreSection = document.getElementById('more-section') as HTMLElement;
 
 
+moreButton.addEventListener('click', () => {
+    moreButton.style.display = 'none';
+    lessButton.style.display = 'flex';
+    moreSection.style.display = 'grid';
+})
+
+lessButton.addEventListener('click', () => {
+    lessButton.style.display = 'none';
+    moreButton.style.display = 'flex';
+    moreSection.style.display = '';
+
+})
+
+
+console.log('test')
