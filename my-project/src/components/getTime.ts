37a72c479261741fe30f7a209bd8dbb1fb861360
Roland Szucs <rolandszucs.private@gmail.com>
2025-08-11 20:14:04 +0200

    seconds and timezone ts / scss

--- my-project/src/components/getTime.ts
+++ my-project/src/components/getTime.ts
@@ -1,6 +1,8 @@
 // === THIS FUNCTION FETCHES THE CURRENT TIME BASED ON THE PROVIDED IP ADDRESS FROM AN EXTERNAL API === //
 
 const currentTime = document.getElementById('current-time-top') as HTMLElement | null;
+const currentSeconds = document.getElementById('current-seconds-top') as HTMLElement | null;
+const currentTimeZone = document.getElementById('current-time-zone-top') as HTMLElement | null;
 
 export async function getTime(ip: string) {
     const url = `https://timeapi.io/api/time/current/ip?ipAddress=${ip}`;
@@ -17,12 +19,16 @@ export async function getTime(ip: string) {
         console.log(result);
 
         // Optionally, update the DOM element with the current time if it exists
-        if (currentTime && result && result.time) {
+        if (currentTime && result && result.time && currentSeconds && result.seconds && currentTimeZone && result.timeZone) {
+
             const time = result.time;
             const seconds = result.seconds;
-            currentTime.textContent = `${time}`;  // Adjust according to API's returned property
-        }
+            const timeZone = result.timeZone;
 
+            currentTime.textContent = `${time}`;  // Adjust according to API's returned property
+            currentSeconds.textContent = `${seconds}`;  // Adjust according to API's returned property
+            currentTimeZone.textContent = `${timeZone}`;  // Adjust according to API's returned property
+        }
 
     } catch (error: any) {
         console.log(error.message);
